1eaa790ce008f25386785a9f13f80a7d416aba3c
jenny80170 <jennifer.malleret@outlook.fr>
2019-11-14 16:08:57 +0100

    Modification de la barre de nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,29 @@ let taille = $('#carrousel li').outerWidth(true);
 $("#carrousel ul").animate({marginLeft: -taille},2000,function(){ $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
 })
 }, 6000);
+$('#home').click(function(event){
+  event.preventDefault();
+  $('#carrousel, #article').hide();
+  $('.home').show();
+  $('#disguise').click(function(event){
+    event.preventDefault();
+    $('#carrousel, #article').hide();
+    $('.disguise').show();
+    $('#sweater').click(function(event){
+      event.preventDefault();
+      $('#carrousel, #article').hide();
+      $('.sweater').show();
+      $('#sock').click(function(event){
+        event.preventDefault();
+        $('#carrousel, #article').hide();
+        $('.sock').show();
+        $('#pajamas').click(function(event){
+          event.preventDefault();
+          $('#carrousel, #article').hide();
+          $('.pajamas').show();
+          $('#accessory').click(function(event){
+            event.preventDefault();
+            $('#carrousel, #article').hide();
+            $('.accessory').show();
+});
 });
